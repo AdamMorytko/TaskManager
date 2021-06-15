a1a6d57352dc45d7ed128c06630dd190786ab554
AdamMorytko <adikmor@gmail.com>
2021-06-15 18:23:27 +0200

    fixed to properly store commas

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -12,9 +12,88 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class TaskManager {
+
+    private static final List<String> menu = Arrays.asList(
+            ConsoleColors.BLUE + "Please select an option:",
+            ConsoleColors.RESET + "1. add",
+            "2. remove",
+            "3. list",
+            "4. exit");
     private static final String fileName = "tasks.csv";
     private static final Path path = Paths.get(fileName);
     private static String[][] tasks = getTasksFromFile();
+
+    public static void main(String[] args) {
+        if (tasks.length == 0){
+            System.out.println(ConsoleColors.BLUE_BOLD + "Hello to the TaskManager! This application will allow you to manage your tasks in a simple way.");
+        }
+        boolean loop = true;
+        while (loop) {
+            showMainMenu();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1":
+                case "add": {
+                    addTask(scanner);
+                    break;
+                }
+                case "2":
+                case "remove": {
+                    removeTask(scanner);
+                    break;
+                }
+                case "3":
+                case "list": {
+                    listTasks();
+                    break;
+                }
+                case "4":
+                case "exit": {
+                    System.out.println(ConsoleColors.RED + "Bye, bye.");
+                    loop = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void showMainMenu() {
+        for (int i = 0; i < menu.size(); i++) {
+            System.out.println(menu.get(i));
+        }
+    }
+
+    private static void addTask(Scanner scanner) {
+        boolean loop = true;
+        String description = "";
+        while (loop){
+            System.out.println("Please add task description:");
+            description = scanner.nextLine();
+            if (description.contains(""+(char)168)){
+                description = "";
+                System.out.println("Description contains an illegal character, please try again.");
+            }else{
+                loop = false;
+            }
+        }
+        System.out.println("Please add task due date");
+        String date = getValidDateFromUser(scanner);
+        String important = "";
+        loop = true;
+        while (loop){
+            System.out.println("Is your task important: true/false");
+            important = scanner.nextLine();
+            if (important.contains("true") || important.contains("false")){
+                loop = false;
+            }
+        }
+        int newLength = tasks.length + 1;
+        tasks = Arrays.copyOf(tasks, newLength);
+        String[] newLine = {description, date, important};
+        tasks[newLength - 1] = newLine;
+        saveToFile(tasks);
+    }
 
     private static String[][] getTasksFromFile() {
         createFile();
@@ -27,8 +106,8 @@ public class TaskManager {
         }
         for (int i = 0; i < list.size(); i++) {
             fileTasks = Arrays.copyOf(fileTasks, fileTasks.length + 1);
-            String[] linia = list.get(i).split(",");
-            fileTasks[i] = linia;
+            String[] line = list.get(i).split(Character.toString((char)168));
+            fileTasks[i] = line;
         }
         return fileTasks;
     }
@@ -43,122 +122,69 @@ public class TaskManager {
         }
     }
 
-    public static void main(String[] args) {
-        while (true) {
-            showMainMenu();
-            Scanner scanner = new Scanner(System.in);
-            String opcja = scanner.nextLine();
-            switch (opcja) {
-                case "add": {
-                    addTask(scanner);
-                    break;
-                }
-                case "remove": {
-                    removeTask(scanner);
-                    break;
-                }
-                case "list": {
-                    listTasks();
-                }
-            }
-            if (opcja.equalsIgnoreCase("exit")) {
-                zapiszDoPliku(tasks);
-                System.out.println(ConsoleColors.RED + "Bye, bye.");
-                break;
-            }
-        }
-    }
-
-    private static void zapiszDoPliku(String[][] tasks) {
-        String[] linia = new String[tasks.length];
+    private static void saveToFile(String[][] tasks) {
+        String[] line = new String[tasks.length];
         for (int i = 0; i < tasks.length; i++) {
-            linia[i] = String.join(",", tasks[i]);
+            line[i] = String.join(Character.toString((char)168), tasks[i]);
         }
 
         try {
-            Files.write(path, Arrays.asList(linia));
+            Files.write(path, Arrays.asList(line));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     private static void listTasks() {
-        for (int i = 0; i < tasks.length; i++) {
-            System.out.print(i + ": ");
-            for (int j = 0; j < tasks[i].length; j++) {
-                System.out.print(tasks[i][j]);
-                if (j != tasks[i].length - 1) {
-                    System.out.print(" ");
-                } else {
-                    System.out.println("");
+        if (tasks.length == 0){
+            System.out.println("You don't have any tasks added.");
+        }else{
+            for (int i = 0; i < tasks.length; i++) {
+                System.out.print(i + ": ");
+                for (int j = 0; j < tasks[i].length; j++) {
+                    System.out.print(tasks[i][j]);
+                    if (j != tasks[i].length - 1) {
+                        System.out.print(" | ");
+                    } else {
+                        System.out.println("");
+                    }
                 }
             }
         }
-        System.out.println("");
     }
 
     private static void removeTask(Scanner scanner) {
-        System.out.println("Please select number to remove.");
-        int index = pobierzLiczbeOdUzytkownika(scanner);
-        while (index < 0 || index > tasks.length - 1) {
-            System.out.println("Index out of bound, enter valid index");
-            index = pobierzLiczbeOdUzytkownika(scanner);
+        if (tasks.length == 0){
+            System.out.println("You don't have any tasks to remove.");
+        }else{
+            System.out.println("Please select number to remove.");
+            int index = getValidNumberFromUser(scanner);
+            while (index < 0 || index > tasks.length - 1) {
+                System.out.println("Index out of bound, enter valid index");
+                index = getValidNumberFromUser(scanner);
+            }
+            tasks = ArrayUtils.remove(tasks, index);
+            saveToFile(tasks);
         }
-        tasks = ArrayUtils.remove(tasks, index);
     }
 
-    private static int pobierzLiczbeOdUzytkownika(Scanner scanner) {
+    private static int getValidNumberFromUser(Scanner scanner) {
         while (!scanner.hasNextInt()) {
             scanner.next();
-            System.out.println("Not a number, please enter a number");
+            System.out.println("Not a number, please enter a number!");
         }
         return scanner.nextInt();
     }
 
-    private static void addTask(Scanner scanner) {
-        System.out.println("Please add task description:");
-        String description = scanner.nextLine();
-        System.out.println("Please add task due date");
-        String date = pobierzDateOdUzytkownika(scanner);
-        System.out.println("Is your task important: true/false");
-        String important = scanner.nextLine();
-        int nowaDlugosc = tasks.length + 1;
-        tasks = Arrays.copyOf(tasks, nowaDlugosc);
-        String[] linia = {description, date, important};
-        tasks[nowaDlugosc - 1] = linia;
-    }
-
-    private static String pobierzDateOdUzytkownika(Scanner scanner) {
+    private static String getValidDateFromUser (Scanner scanner) {
         System.out.println("Use dd-MM-yyyy format");
-        String sdate = scanner.nextLine();
-        while (!isValidDate(sdate)) {
+        String stringDate = scanner.nextLine();
+        while (!isValidDate(stringDate)) {
             System.out.println("Please use correct date and format!");
-            sdate = scanner.nextLine();
+            stringDate = scanner.nextLine();
         }
-//        while (!isInTheFuture(sdate)){
-//            System.out.println("Date can't be in the past!");
-//            sdate = scanner.nextLine();
-//            while (!isValidDate(sdate)){
-//                System.out.println("Please use correct date and format!");
-//                sdate = scanner.nextLine();
-//            }
-//        }
-        return sdate;
+        return stringDate;
     }
-
-//    private static boolean isInTheFuture(String sdate) {
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-//        Date taskDate = null;
-//        try {
-//            taskDate = dateFormat.parse(sdate);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        Date current = dateFormat.parse();
-//        System.out.println(current);
-//        System.out.println(current.after(taskDate));
-//        return current.after(taskDate);
-//    }
 
     public static boolean isValidDate(String date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
@@ -169,18 +195,5 @@ public class TaskManager {
             return false;
         }
         return true;
-    }
-
-    private static void showMainMenu() {
-        List<String> menuOptions = new ArrayList<>();
-        menuOptions.add(ConsoleColors.BLUE + "Please select an option:");
-        menuOptions.add(ConsoleColors.RESET + "add");
-        menuOptions.add("remove");
-        menuOptions.add("list");
-        menuOptions.add("exit");
-        for (int i = 0; i < menuOptions.size(); i++) {
-            System.out.println(menuOptions.get(i));
-        }
-
     }
 }
